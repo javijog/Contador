49c8992b15a6b506713503163a98733d07610963
jmartinezm <jmartinezm@hiberus.com>
2022-08-17 03:19:22 +0200

    Diseño Portada completado

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2022, // required
-	month: 6, // required
-	day: 23, // required
-	hours: 1, // Default is 0 [0-23] integer
+	month: 9, // required
+	day: 19, // required
+	hours: 2, // Default is 0 [0-23] integer
 	minutes: 1, // Default is 0 [0-59] integer
 	seconds: 1, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
